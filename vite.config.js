66593e17f636bd8c8a57ca613b3de709e18a5d6f
SkylarDV <dorienvermeulen82@gmail.com>
2026-01-07 16:24:00 +0100

    fix: linked seperate page

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        configurator: path.resolve(__dirname, 'configurator.html'),
+        profile: path.resolve(__dirname, 'profile.html'),
+      },
+    },
+  },
+});
